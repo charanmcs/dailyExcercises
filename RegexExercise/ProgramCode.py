63a045e9e9d131b8492587486590a1cf20431fea
charanmcs <77063780+charanmcs@users.noreply.github.com>
2022-04-04 10:11:29 +0530

    Update ProgramCode.py

--- RegexExercise/ProgramCode.py
+++ RegexExercise/ProgramCode.py
@@ -1,17 +1,16 @@
 from importlib.resources import contents
 import sys
-import pandas as pd
-import numpy as np
 import regex as re
 f=open(sys.argv[1],'r',encoding='utf-8')
 contents = f.read()
-pattern1 = r"[£₹$][0-9]{1,}"
+f.close()
+pattern1 = re.compile(r"(?<!\S)[£₹$][0-9]{1,}(\.\d+)?(?!\S)")
 pattern2 = re.compile(r"(([0-1]\d\/[0-3]\d\/|([0-3]\d\/[0-1]\d\/))\d\d(\d{2})?)")
 pattern3 = re.compile("(\d*(1st|2nd|3rd|[04-9]th))|[a-zA-Z]*(first|eleventh|second|twe(l(ve|th)|ntyth)|thir(teenth|d|tyth)|four((teen)?th|tyth)|fif((teen)?th|tyth)|six((teen)?th|tyth)|seven((teen)?th|tyth)|eigh((teen)?th|tyth)|nine((teen)?th|tyth)|tenth)")
 pattern4 = r"\b[aeiouAEIOU][a-zA-Z]{3}\b"
 print("Currencie Matches : ",end="")
-for match in re.findall(pattern1,contents):
-    print(match,end=" ")
+for match in re.finditer(pattern1,contents):
+    print(match.group(0),end=" ")
 print("\nDate Matches : ",end="")
 for match in re.finditer(pattern2,contents):
     print(match.group(0),end=" ")
@@ -21,4 +20,3 @@ for match in re.finditer(pattern3,contents):
 print("\nFour letter words begining with vowels : ",end="")
 for match in re.findall(pattern4,contents):
     print(match,end=" ")
-f.close()
