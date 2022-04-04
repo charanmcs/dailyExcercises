4fe85f3188affac2cc6a0919f6766b5f16caf679
charanmcs <77063780+charanmcs@users.noreply.github.com>
2022-04-04 09:41:54 +0530

    Add files via upload

--- RegexExercise/ProgramCode.py
+++ RegexExercise/ProgramCode.py
@@ -8,16 +8,17 @@ contents = f.read()
 pattern1 = r"[£₹$][0-9]{1,}"
 pattern2 = re.compile(r"(([0-1]\d\/[0-3]\d\/|([0-3]\d\/[0-1]\d\/))\d\d(\d{2})?)")
 pattern3 = re.compile("(\d*(1st|2nd|3rd|[04-9]th))|[a-zA-Z]*(first|eleventh|second|twe(l(ve|th)|ntyth)|thir(teenth|d|tyth)|four((teen)?th|tyth)|fif((teen)?th|tyth)|six((teen)?th|tyth)|seven((teen)?th|tyth)|eigh((teen)?th|tyth)|nine((teen)?th|tyth)|tenth)")
-pattern4 = re.compile("\s[aeiouAEIOU][a-zA-Z]{3}(\s|\n)")
+pattern4 = r"\b[aeiouAEIOU][a-zA-Z]{3}\b"
+print("Currencie Matches : ",end="")
 for match in re.findall(pattern1,contents):
     print(match,end=" ")
-print()
+print("\nDate Matches : ",end="")
 for match in re.finditer(pattern2,contents):
     print(match.group(0),end=" ")
-print()
+print("\nOrder or Cardinalities Matches : ",end="")
 for match in re.finditer(pattern3,contents):
     print(match.group(0),end=" ")
-print()
-for match in re.finditer(pattern4,contents):
-    print(match.group(0),end=" ")
+print("\nFour letter words begining with vowels : ",end="")
+for match in re.findall(pattern4,contents):
+    print(match,end=" ")
 f.close()
